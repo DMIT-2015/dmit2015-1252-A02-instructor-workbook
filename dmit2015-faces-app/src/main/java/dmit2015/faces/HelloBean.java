61e03dc9c2c1a5fff062941d9b5b7a0a7630ec0b
Sam Wu <swu@nait.ca>
2026-01-20 13:55:26 -0700

    Added beans.xml, hello.xhtml, and HelloBean.java

--- dmit2015-faces-app/src/main/java/dmit2015/faces/HelloBean.java
+++ dmit2015-faces-app/src/main/java/dmit2015/faces/HelloBean.java
@@ -2,8 +2,34 @@ package dmit2015.faces;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
+import net.datafaker.Faker;
+import org.omnifaces.util.Messages;
 
-@Named
+@Named("helloRequest")
 @RequestScoped
 public class HelloBean {
+
+    @NotBlank(message = "Username is required.")
+    private String userInput;
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public String getMessage() {
+        return "Hello, " + userInput;
+    }
+
+    public String submit() {
+
+        var faker = new Faker();
+        Messages.addGlobalInfo("Your favorite pokemon is {0}",
+                faker.pokemon().name());
+        return null; // or navigation outcome
+    }
 }
